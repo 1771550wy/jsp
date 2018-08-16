500bea9c8400c5f6127a1f9258274e96d4a3a1c3
1771550wy <38021966+1771550wy@users.noreply.github.com>
2018-08-16 10:50:11 +0800

    Create L02.m

---
+++ L02.m
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+function changeImage()
+{
+element=document.getElementById('myimage')
+if (element.src.match("bulbon"))
+  {
+  element.src="../...jpg1";
+  }
+else
+  {
+  element.src="../...jpg2";
+  }
+}
+</script>
+
+<img id="myimage" onclick="changeImage()" src="../...jpg2">
+
+<p>点击更换图片</p>
+
+</body>
+</html>
